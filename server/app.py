e081e146f4940f2ea554845e41bec71693f28e48
madhusri829 <madhusri9981@gmail.com>
2026-03-29 21:53:17 +0530

    Switch Docker to Server mode

--- server/app.py
+++ server/app.py
@@ -1,7 +1,4 @@
-def main():
-    import uvicorn
-    # This tells the server to run on port 8000
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
-
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    # This starts the server on port 8000
+    uvicorn.run(app, host="0.0.0.0", port=8000)
